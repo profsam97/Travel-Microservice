2932e19e5740d2ca9e72969a7f4c829de7f0fa47
profsam97 <proftoby4real@hotmail.com>
2024-05-29 10:24:53 +0100

    updated interface

--- Micro-services/auths-service/src/interfaces/request.interface.ts
+++ Micro-services/auths-service/src/interfaces/request.interface.ts
@@ -1,8 +1,6 @@
-// custom-request.interface.ts
 
 import { Request } from 'express';
 
-// Extend the Request interface to include the user property
 declare module 'express' {
   interface Request {
     user: any; // Define the type of the user property
