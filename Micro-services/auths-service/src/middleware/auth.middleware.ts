7cdadb5aa928fec830d579568ac309be3db0fa9d
profsam97 <proftoby4real@hotmail.com>
2024-05-29 10:25:35 +0100

    updated auth middleware

--- Micro-services/auths-service/src/middleware/auth.middleware.ts
+++ Micro-services/auths-service/src/middleware/auth.middleware.ts
@@ -1,4 +1,3 @@
-// auth.middleware.ts
 
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
@@ -6,7 +5,6 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    // Check if user is authenticated (you can customize this logic)
     if (req.isAuthenticated()) {
       // User is authenticated, prevent access to the signup route
       return res.status(403).json({ message: 'You are already signed in' });
